c504335ac40f53b0decd3637c27508d96ebb01ca
Meir Blachman <meirblachman@gmail.com>
2025-05-14 18:49:37 +0300

    Feature/console logs (#21)
    * feature: CLI logs
    
    * add LoggerUsageExtractorTests

---
+++ test/LoggerUsage.Tests/LoggerUsageExtractorTests.cs
@@ -0,0 +1,42 @@
+namespace LoggerUsage.Tests;
+
+public class LoggerUsageExtractorTests
+{    
+    [Fact]
+    public async Task BasicTestWithLogMethodAndLoggerMessageAttribute()
+    {
+        // Arrange
+        var compilation = await TestUtils.CreateCompilationAsync(@"using Microsoft.Extensions.Logging;
+namespace TestNamespace;
+
+public partial class TestClass
+{
+    public void TestMethod(ILogger logger)
+    {
+        logger.LogInformation(""Test message"");
+        TestLogMethod(logger);
+    }
+
+    [LoggerMessage(
+        LogLevel.Information,
+        ""Test message""
+    )]
+    private static partial void TestLogMethod(ILogger logger);
+}
+
+partial class TestClass
+{
+    private static partial void TestLogMethod(ILogger logger) {}
+}");
+        var extractor = new LoggerUsageExtractor();
+
+        // Act
+        var result = extractor.ExtractLoggerUsages(compilation);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(2, result.Count);
+    }
+
+}
+
