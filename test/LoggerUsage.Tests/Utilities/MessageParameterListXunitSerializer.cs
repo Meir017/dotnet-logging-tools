4b3d8ec35621d67c9fead2c244d645c1b9b62de1
Meir Blachman <meirblachman@gmail.com>
2025-05-16 11:53:39 +0300

    feature: enhance message parameter extraction logic and improve serialization in LoggerMessageAttributeAnalyzer and tests

--- test/LoggerUsage.Tests/Utilities/MessageParameterListXunitSerializer.cs
+++ test/LoggerUsage.Tests/Utilities/MessageParameterListXunitSerializer.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics.CodeAnalysis;
+using System.Text.Json;
 using LoggerUsage.Models;
 using Microsoft.CodeAnalysis;
 using Xunit.Sdk;
@@ -7,25 +8,7 @@ namespace LoggerUsage.Tests;
 
 public class MessageParameterListXunitSerializer : IXunitSerializer
 {
-    public object Deserialize(Type type, string serializedValue)
-    {
-        if (type == typeof(List<MessageParameter>))
-        {
-            var parts = serializedValue.Split('|');
-            var messageParameters = new List<MessageParameter>(parts.Length);
-            for (int i = 0; i < parts.Length; i++)
-            {
-                var subParts = parts[i].Split(',');
-                var name = subParts[0];
-                var typeName = subParts[1];
-                var kind = subParts[2];
-                messageParameters.Add(new MessageParameter(name, typeName, kind));
-            }
-            return messageParameters;
-        }
-
-        throw new NotSupportedException($"Cannot deserialize type {type}");
-    }
+    public object Deserialize(Type type, string serializedValue) => JsonSerializer.Deserialize(serializedValue, type)!;
 
     public bool IsSerializable(Type type, object? value, [NotNullWhen(false)] out string? failureReason)
     {
@@ -39,13 +22,5 @@ public class MessageParameterListXunitSerializer : IXunitSerializer
         return false;
     }
 
-    public string Serialize(object value)
-    {
-        if (value is List<MessageParameter> messageParameters)
-        {
-            return string.Join("|", messageParameters.Select(mp => $"{mp.Name},{mp.Type},{mp.Kind}"));
-        }
-
-        throw new NotSupportedException($"Cannot serialize type {value.GetType()}");
-    }
+    public string Serialize(object value) => JsonSerializer.Serialize(value);
 }
